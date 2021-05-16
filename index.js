6693bf6bf0a93229534f012afc3a4c5ab2afd6d4
JASI GRACE I <67458870+jasigrace@users.noreply.github.com>
2021-05-16 08:41:44 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Dice1
+
+var randomNumber1 = Math.floor(Math.random()*6) + 1; // To Get Dice Number 1-6
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceImage1);
+
+// Dice2
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomDiceImage2);
+
+// Winning Player
+
+if(randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+}
+else if(randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
